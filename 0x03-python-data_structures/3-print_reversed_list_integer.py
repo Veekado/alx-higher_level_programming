8052590acbba0245cc5fe0e8dd0947466b4194bb
Imeh Victor <imohimeh33@gmail.com>
2023-01-10 14:47:38 -0800

    updated some scripts

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for x in reversed(my_list):
-        print("{:d}".format(x))
+    if not my_list:
+        return None
+    else:
+        for x in reversed(my_list):
+            print("{:d}".format(x))
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,6 @@
 def no_c(my_string):
     new_string = ""
     for ch in my_string:
-        if ch != 'c' and ch != 'C':
+        if ch != "c" and ch != "C":
             new_string += ch
     return new_string
-
-
