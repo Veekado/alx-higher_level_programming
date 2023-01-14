462e4c7544b73020a253e50c9727471dab5c2b74
Imeh Victor <imohimeh33@gmail.com>
2023-01-13 17:46:57 -0800

    more corrections

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-
-
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if not a_dictionary:
         return None
-    ret = list(a_dictionary.keys())[0]
-    big = a_dictionary[ret]
-    for k, v in a_dictionary.items():
-        if v > big:
-            big = v
-            ret = k
-    return (ret)
+
+    dict_keys = list(a_dictionary)
+    if len(dict_keys) == 0:
+        return None
+    else:
+        biggest = a_dictionary[dict_keys[0]]
+        name = dict_keys[0]
+        for k, v in a_dictionary.items():
+            if v > biggest:
+                biggest = v
+                name = k
+        return name
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    return (len(a_dictionary))
+    count = 0
+    for k, v in a_dictionary.items():
+        count += 1
 
+    return count
