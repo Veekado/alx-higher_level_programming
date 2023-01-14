5dc8e45e101c94a30cf11562b5284fd0b3946d6c
Imeh Victor <imohimeh33@gmail.com>
2023-01-13 17:25:27 -0800

    Romans wasnt easy

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+
+    avg = 0
+    size= 0
+
+    for item in my_list:
+        avg += item[0] * item[1]
+        size += item[1]
+
+    return (avg / size)
